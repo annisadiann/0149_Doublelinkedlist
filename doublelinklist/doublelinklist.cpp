35f669be92fafdd4403d6b02261202519864a673
annisadiann <annisadian0412@gmail.com>
2024-05-05 09:11:50 +0700

    menambahkan prosedur void addNode

--- doublelinklist/doublelinklist.cpp
+++ doublelinklist/doublelinklist.cpp
@@ -11,6 +11,16 @@ struct Node
 };
 
 Node* START = NULL;
+
+void addNode() {
+    Node* newNode = new Node(); //step 1 crate a new node
+    cout << "\nEnter the roll number of the student : ";
+    cin >> newNode->noMhs;      //Assign value to the data field of the new node
+    cout << "\nEnter the name of the student : ";
+    cin >> newNode->name;       //Assign value to the data field of the new node
+
+    //Insert the new node in the list
+} 
  
 
 int main()
